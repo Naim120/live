00e119d28c9681183abf1d521f48ca3e80b745fe
sk naim <sknaimuddin234@gmail.com>
2023-03-23 20:29:37 +0530

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,8 @@
+sudo apt install wget
+wget https://launchpad.net/ubuntu/+archive/primary/+sourcefiles/ffmpeg/7:5.1.1-1ubuntu2.1/ffmpeg_5.1.1.orig.tar.xz
+tar -xf ffmpeg_5.1.1.orig.tar.xz
+cd ffmpeg_5.1.1
+./configure
+make
+make install
+ffmpeg --version
